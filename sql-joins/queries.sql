f2ea218f8860106f5d91f6b042e6ef7a5ae0cf76
nickbutani <naitikbu@gmail.com>
2024-04-11 16:41:36 -0400

    joins commit

---
+++ sql-joins/queries.sql
@@ -0,0 +1,22 @@
+-- write your queries here
+
+--  SELECT * FROM owners o FULL OUTER JOIN vehicles v ON o.id = v.owner_id;
+--  SELECT o.first_name, o.last_name, COUNT(o.id) FROM owners o LEFT JOIN vehicles v ON o.id = v.owner_id GROUP BY o.first_name, o.last_name;
+-- 
+-- SELECT
+--     o.first_name,
+--     o.last_name,
+--     ROUND(AVG(v.price)) AS average_price,
+--     COUNT(v.id) AS vehicle_count
+-- FROM
+--     owners o
+-- JOIN
+--     vehicles v ON o.id = v.owner_id
+-- GROUP BY
+--     o.first_name,
+--     o.last_name
+-- HAVING
+--     COUNT(v.id) > 1
+--     AND AVG(v.price) > 10000
+-- ORDER BY
+--     o.first_name DESC;
