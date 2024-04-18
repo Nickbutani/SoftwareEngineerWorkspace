c3388f1c45dbec92d246bad3a742c0c7a92dca4e
nickbutani <naitikbu@gmail.com>
2024-04-18 19:02:32 -0400

    cupcake update

--- flask-cupcakes/app.py
+++ flask-cupcakes/app.py
@@ -2,7 +2,18 @@ from flask import Flask, request, jsonify, render_template
 
 from models import db, connect_db, Cupcake
 
+from traceback import format_exc
+
+
 app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*') # Allows all domains
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH')
+    return response
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -32,25 +43,23 @@ def list_cupcakes():
 
 @app.route("/api/cupcakes", methods=["POST"])
 def create_cupcake():
-    """Add cupcake, and return data about new cupcake.
-
-    Returns JSON like:
-        {cupcake: [{id, flavor, rating, size, image}]}
-    """
-
     data = request.json
 
-    cupcake = Cupcake(
-        flavor=data['flavor'],
-        rating=data['rating'],
-        size=data['size'],
-        image=data['image'] or None)
+    try:
+        cupcake = Cupcake(
+            flavor=data.get('flavor'),
+            rating=data.get('rating'),
+            size=data.get('size'),
+            image=data.get('image') or None)
 
-    db.session.add(cupcake)
-    db.session.commit()
+        db.session.add(cupcake)
+        db.session.commit()
 
-    # POST requests should return HTTP status of 201 CREATED
-    return (jsonify(cupcake=cupcake.to_dict()), 201)
+        return jsonify(cupcake=cupcake.to_dict()), 201
+    except  Exception as exc:
+        print(f"An error occurred: {exc}\n{format_exc()}")
+        db.session.rollback()
+        return jsonify(error="Internal server error"), 500
 
 
 @app.route("/api/cupcakes/<int:cupcake_id>")
