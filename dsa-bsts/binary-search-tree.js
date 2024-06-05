835f0b5dd0e243cb81721bee6210b0e08f6efb02
nickbutani <naitikbu@gmail.com>
2024-06-05 15:45:39 -0400

    bst commit

---
+++ dsa-bsts/binary-search-tree.js
@@ -0,0 +1,277 @@
+class Node {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BinarySearchTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  /** insert(val): insert a new node into the BST with value val.
+   * Returns the tree. Uses iteration. */
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  /** insertRecursively(val): insert a new node into the BST with value val.
+   * Returns the tree. Uses recursion. */
+
+  insertRecursively(val, current = this.root) {
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
+
+    if (val < current.val) {
+      if (current.left === null) {
+        current.left = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, current.left);
+    } else {
+      if (current.right === null) {
+        current.right = new Node(val);
+        return this;
+      }
+      return this.insertRecursively(val, current.right);
+    }
+
+  }
+
+  /** find(val): search the tree for a node with value val.
+   * return the node, if found; else undefined. Uses iteration. */
+
+  find(val) {
+    if (!this.root) return undefined;
+    let current = this.root;
+    while (current) {
+      if (val === current.val) return current;
+      if (val < current.val) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return undefined;
+  }
+
+  /** findRecursively(val): search the tree for a node with value val.
+   * return the node, if found; else undefined. Uses recursion. */
+
+  findRecursively(val) {
+    if (!this.root) return undefined;
+    function findNode(node) {
+      if (!node) return undefined;
+      if (val === node.val) return node;
+      if (val < node.val) {
+        return findNode(node.left);
+      } else {
+        return findNode(node.right);
+      }
+    }
+    return findNode(this.root);
+  }
+
+  /** dfsPreOrder(): Traverse the array using pre-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsPreOrder() {
+    let visited = [];
+    function traverse(node) {
+      visited.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
+
+  /** dfsInOrder(): Traverse the array using in-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsInOrder() {
+    let visited = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      visited.push(node.val);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return visited;
+  }
+
+  /** dfsPostOrder(): Traverse the array using post-order DFS.
+   * Return an array of visited nodes. */
+
+  dfsPostOrder() {
+    let visited = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visited.push(node.val);
+    }
+    traverse(this.root);
+    return visited;
+  }
+
+  /** bfs(): Traverse the array using BFS.
+   * Return an array of visited nodes. */
+
+  bfs() {
+    let visited = [];
+    let queue = [];
+    let current = this.root;
+    queue.push(current);
+    while (queue.length) {
+      current = queue.shift();
+      visited.push(current.val);
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+    }
+    return visited;
+  }
+
+  /** Further Study!
+   * remove(val): Removes a node in the BST with the value val.
+   * Returns the removed node. */
+
+  remove(val) {
+    let nodeToRemove = this.root;
+    let parent;
+
+    while (nodeToRemove.val !== val) {
+      parent = nodeToRemove;
+      if (val < nodeToRemove.val) {
+        nodeToRemove = nodeToRemove.left;
+      } else {
+        nodeToRemove = nodeToRemove.right;
+      }
+    }
+
+    if (nodeToRemove !== this.root) {
+      if (nodeToRemove.left === null && nodeToRemove.right === null) {
+        if (parent.left === nodeToRemove) {
+          parent.left = null;
+        } else {
+          parent.right = null;
+        }
+      } else if (nodeToRemove.left !== null && nodeToRemove.right !== null) {
+        let rightParent = nodeToRemove;
+        let right = nodeToRemove.right;
+        if (right.left === null) {
+          right.left = nodeToRemove.left;
+          if (parent.left === nodeToRemove) {
+            parent.left = right;
+          } else {
+            parent.right = right;
+          }
+        } else {
+          while (right.left !== null) {
+            rightParent = right;
+            right = right.left;
+          }
+          if (parent.left === nodeToRemove) {
+            parent.left.val = right.val;
+          } else {
+            parent.right.val = right.val;
+          }
+          if (right.right !== null) {
+            rightParent.left = right.right;
+          } else {
+            rightParent.left = null;
+          }
+        }
+      } else {
+        if (parent.left === nodeToRemove) {
+          if (nodeToRemove.right === null) {
+            parent.left = nodeToRemove.left;
+          } else {
+            parent.left = nodeToRemove.right;
+          }
+        } else {
+          if (nodeToRemove.right === null) {
+            parent.right = nodeToRemove.left;
+          } else {
+            parent.right = nodeToRemove.right;
+          }
+        }
+      }
+    }
+    return nodeToRemove;
+  }
+
+  /** Further Study!
+   * isBalanced(): Returns true if the BST is balanced, false otherwise. */
+
+  isBalanced() {
+    function checkHeight(node) {
+      if (!node) return 0;
+      let leftHeight = checkHeight(node.left);
+      if (leftHeight === -1) return -1;
+      let rightHeight = checkHeight(node.right);
+      if (rightHeight === -1) return -1;
+      if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+      return Math.max(leftHeight, rightHeight) + 1;
+    }
+    return checkHeight(this.root) !== -1;
+  }
+
+  /** Further Study!
+   * findSecondHighest(): Find the second highest value in the BST, if it exists.
+   * Otherwise return undefined. */
+
+  findSecondHighest() {
+    if (!this.root) return undefined;
+    let current = this.root;
+    let parent = null;
+    while (current) {
+      if (!current.right && current.left) {
+        if (parent) {
+          return parent.val;
+        } else {
+          return undefined;
+        }
+      }
+      if (!current.right && !current.left) {
+        if (parent) {
+          return parent.val;
+        } else {
+          return undefined;
+        }
+      }
+      if (!current.right.right && !current.right.left) {
+        return current.val;
+      }
+      parent = current;
+      current = current.right;
+    }
+  }
+}
+
+module.exports = BinarySearchTree;
